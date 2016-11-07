0e991b15752f39b4bca4ac6975bbb84b116e0bd7
Jay Reynolds <reynoldsjay@gatech.edu>
2016-11-06 22:14:31 -0500

    Video cropping.

--- script/prepare_training_data.sh
+++ script/prepare_training_data.sh
@@ -1,17 +1,22 @@
-VIDEO_PATH='../data/FingerBending4GB.mp4'
+VIDEO_PATH='../data/finger_mason_bending_1.avi'
 GLOVE_PATH='../data/finger_bending_mason_1.txt'
 PROTO_BUF_PATH='../data/protobuf_finger_bending_mason_1_ds36'
 # The timestamp of the start frame
 start_time='1476463978.558'
 # The start frame is the first valid frame which is decided by observing manually
-start_frame='143'
+start_frame='111'
 # The frame rate of the video
-frame_rate='30'
+frame_rate='23'
 # The size of each of the buffer
 buf_size='1000'
-# The number of the buffer that you want to output, 
+# The number of the buffer that you want to output,
 # if you want to process all the data, set buf_num='-1'
-buf_num='1' 
+buf_num='-1'
+# cropping number of pixels
+top_crop='53'
+bottom_crop='18'
+left_crop='45'
+right_crop='169'
 # Preprocessing mode
 # Each processing step is divided by '#', e.g. [step1]#[step2]#...
 # The before comma part is the name of processing, including: binary, canny, downscale
@@ -24,7 +29,4 @@ proc_mode='downscale,0.05#binary,-1'
 
 python ../code/extract_data.py ${VIDEO_PATH} ${GLOVE_PATH} ${PROTO_BUF_PATH} \
 	${start_time} ${start_frame} ${frame_rate} \
-	${buf_size} ${buf_num} ${proc_mode} 2> ../log/extract_data.log
-
-
-
+	${buf_size} ${buf_num} ${proc_mode} ${top_crop} ${bottom_crop} ${left_crop} ${right_crop} 2> ../log/extract_data.log
