a470c533ef80f42e79c4426e804de46ff3aae02d
meowoodie <woodielove@163.com>
2016-11-15 21:55:32 +0000

    add cnn

--- script/test_new_model.sh
+++ script/test_new_model.sh
@@ -1,8 +1,10 @@
-protobuf_data_file_name='protobuf_fingerbendingmason1_36_binary_all'
+protobuf_data_file_name='protobuf_fingerbendingmason1_36_all'
 model_name='dbn'
 model_file_name='model.test_dbn'
-hidden_layers='30'
+hidden_layers='50'
 train_test_ratio='9'
+learning_rate='.00005'
+model='cnn'
 
 # Train the model
 python ../code/train.py \
@@ -10,10 +12,11 @@ python ../code/train.py \
 	${model_file_name} \
 	${hidden_layers} \
 	${train_test_ratio} \
+	${learning_rate} \
+	${model} \
 	> ../log/train.log 2>&1
 
 python ../code/predict.py offline_test \
 	${model_name} ${model_file_name} ${hidden_layers} \
-	${protobuf_data_file_name} ${train_test_ratio} > contrast.txt
-
-cat contrast.txt | python ../code/visualize.py vis_result 
+	${protobuf_data_file_name} ${train_test_ratio} | \
+python ../code/visualize.py vis_result 
