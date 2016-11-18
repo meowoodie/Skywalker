98767ab3458193c25fc534764d7549145948428d
meowoodie <woodielove@163.com>
2016-11-18 03:18:06 +0000

    change the path of the output

--- script/update_protobuf.sh
+++ script/update_protobuf.sh
@@ -1,4 +1,4 @@
 SRC_DIR='..'
-DST_DIR='../code'
+DST_DIR='../code/lib'
 PROTOBUF_FILE_NAME='ultrasound_with_gesture.proto'
 protoc -I=${SRC_DIR} --python_out=${DST_DIR} ${SRC_DIR}/${PROTOBUF_FILE_NAME}
